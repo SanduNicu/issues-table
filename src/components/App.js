3e8191aa07c750150c268d2b1b57a033542c9b04
SanduNicu <sandu.nicu94@gmail.com>
2019-05-08 20:28:07 +0300

    fetched data

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,36 @@
-import React, { Component } from "react";
+import React from 'react';
 
-import "styles/index.scss";
+const url = 'https://api.github.com/repos/facebook/react/issues';
 
-class App extends Component {
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+
+  componentDidMount() {
+    this.fetchData()    
+  }
+
+  fetchData = () => {
+    const promise = new Promise((res, rej) => {
+      const xmlHttp = new XMLHttpRequest();
+      xmlHttp.onreadystatechange = function() { 
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+          res(xmlHttp.responseText)
+      }
+      xmlHttp.open("GET", url, true); // true for asynchronous 
+      xmlHttp.send(null);
+    })
+
+    promise.then(res => console.log(res))
+  }
+
   render() {
     return (
       <div>
-        <h1>Boilerplate</h1>
+        Placeholder
       </div>
     );
   }
